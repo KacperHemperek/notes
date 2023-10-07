f995124ceda6bb342d147ba9e0533555c3b8ba6f
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-10-07 11:24:51 +0200

    feat: add Note type to db models

---
+++ src/models/notes.ts
@@ -0,0 +1,10 @@
+export type Note = {
+    id: string
+    title: string
+    question: string
+    answer: string
+    hints?: string[]
+    tags?: string[]
+    createdAt: string
+    updatedAt: string
+}
