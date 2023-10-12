0cdb183ca2b9b2d14b2f0502f362cfb252979f8f
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-10-12 23:49:35 +0200

    chore: add fixme for navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,20 +1,20 @@
 import { useAuth } from '~/context/auth-context'
-
+// FIXME: padding not matching with the rest of the app probably should use a container instead
 export function Navigation() {
-    const { user, signOut } = useAuth()
+  const { user, signOut } = useAuth()
 
-    return (
-        <nav className="flex w-screen p-4 bg-slate-900 justify-between items-center sticky top-0 left-0">
-            <h2 className="text-lg font-semibold">Notes</h2>
-            {user && user.photoURL && (
-                <div
-                    className="flex w-8 h-8 rounded-full overflow-hidden"
-                    onClick={signOut}
-                >
-                    <img src={user.photoURL} alt="Profile image" />
-                </div>
-            )}
-            {!user && <div className="w-8 h-8"></div>}
-        </nav>
-    )
+  return (
+    <nav className="flex w-screen p-4 bg-slate-900 justify-between items-center sticky top-0 left-0">
+      <h2 className="text-lg font-semibold">Notes</h2>
+      {user && user.photoURL && (
+        <div
+          className="flex w-8 h-8 rounded-full overflow-hidden"
+          onClick={signOut}
+        >
+          <img src={user.photoURL} alt="Profile image" />
+        </div>
+      )}
+      {!user && <div className="w-8 h-8"></div>}
+    </nav>
+  )
 }
