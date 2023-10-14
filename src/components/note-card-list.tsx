5667f087b68c3acb6366c2a729e8ad654fcf05f1
Kacper Hemperek <kacper.hemperek@o2.pl>
2023-10-14 12:30:39 +0200

    feat: delete note list with a prompt

--- src/components/note-card-list.tsx
+++ src/components/note-card-list.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NOTES_LIST_TABLE, NoteList } from '~/models/notes'
 import { AnimatePresence, motion } from 'framer-motion'
 import { AddNoteDialog } from '~/components/add-note-dialog'
@@ -7,6 +7,16 @@ import { deleteNotesList } from '~/api/delete-note'
 import { useQueryClient } from '@tanstack/react-query'
 import { useAuth } from '~/context/auth-context'
 import { Loader2, Plus, X } from 'lucide-react'
+import {
+  DialogContent,
+  DialogDescription,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from '~/components/common/dialog'
+import { NotesInput } from '~/components/inputs'
+import { useForm } from 'react-hook-form'
+import { NotesButton } from '~/components/common/button'
 
 const parent = {
   initial: {
@@ -77,6 +87,17 @@ export function NoteCard({ note }: { note: NoteList }) {
   const queryClient = useQueryClient()
   const { user } = useAuth()
 
+  const [open, setOpen] = useState(false)
+
+  const {
+    formState: { errors, isValid },
+    register,
+    reset,
+    handleSubmit,
+  } = useForm<{ title: string }>({
+    mode: 'all',
+  })
+
   // TODO: handler errors with a toast and bring back old data
   const { mutate: deleteMutation } = useMutation({
     mutationFn: deleteNotesList,
@@ -91,33 +112,87 @@ export function NoteCard({ note }: { note: NoteList }) {
     },
   })
 
+  function onSubmit() {
+    deleteMutation({ id: note.id, user })
+    setOpen(false)
+  }
+
   return (
-    <motion.div
-      layout={'position'}
-      variants={item}
-      key={note.id}
-      className="flex flex-col gap-4 p-4 rounded-lg border border-slate-800"
-    >
-      <div className="flex justify-between">
-        <h3 className="text-xl font-semibold">{note.title}</h3>
-        <button onClick={() => deleteMutation({ id: note.id, user })}>
-          <X className="w-6 h-6 text-slate-50 hover:text-red-500 transition-colors" />
-        </button>
-      </div>
-      <p>{note.description}</p>
-      {note.tags && (
-        <div className="flex flex-row flex-wrap gap-2">
-          {note.tags.map((tag) => (
-            <span
-              key={tag}
-              className="px-2 py-1 text-sm text-white bg-slate-800 rounded-md whitespace-nowrap"
-            >
-              {tag}
-            </span>
-          ))}
+    <DialogRoot onOpenChange={setOpen} open={open}>
+      <motion.div
+        layout={'position'}
+        variants={item}
+        key={note.id}
+        className="flex flex-col gap-4 p-4 rounded-lg border border-slate-800"
+      >
+        <div className="flex justify-between">
+          <h3 className="text-xl font-semibold">{note.title}</h3>
+          <DialogTrigger asChild>
+            <button>
+              <X className="w-6 h-6 text-slate-50 hover:text-red-500 transition-colors" />
+            </button>
+          </DialogTrigger>
         </div>
-      )}
-    </motion.div>
+        <p>{note.description}</p>
+        {note.tags && (
+          <div className="flex flex-row flex-wrap gap-2">
+            {note.tags.map((tag) => (
+              <span
+                key={tag}
+                className="px-2 py-1 text-sm text-white bg-slate-800 rounded-md whitespace-nowrap"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        )}
+      </motion.div>
+      <DialogContent
+        overlayClick={() => {
+          reset()
+          setOpen(false)
+        }}
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <DialogTitle>Are You Sure</DialogTitle>
+        <DialogDescription>
+          When you delete this note, all of its data will be permanently removed
+        </DialogDescription>
+        <DialogDescription>
+          To remove this note, type <strong>{note.title}</strong> in the field
+        </DialogDescription>
+
+        <div className="grid gap-6">
+          <NotesInput
+            errors={errors}
+            label="Repeat title"
+            register={register('title', {
+              required: {
+                value: true,
+                message: 'Please repeat the title',
+              },
+              pattern: {
+                value: new RegExp(note.title),
+                message: 'The title is not correct',
+              },
+            })}
+          />
+          <NotesButton disabled={!isValid} variant="danger" type="submit">
+            Delete Permanently
+          </NotesButton>
+          <NotesButton
+            variant="secondary"
+            type="reset"
+            onClick={() => {
+              reset()
+              setOpen(false)
+            }}
+          >
+            Cancel
+          </NotesButton>
+        </div>
+      </DialogContent>
+    </DialogRoot>
   )
 }
 
